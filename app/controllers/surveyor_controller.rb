012db8056d5c8f57a33e943d3f20d9d816f9d8ab
Ben Foxall <benfoxall@gmail.com>
2013-05-11 14:50:21 +0100

    use application layout for surveyor

---
+++ app/controllers/surveyor_controller.rb
@@ -0,0 +1,8 @@
+# Surveyor Controller allows a user to take a survey. It is semi-RESTful since it does not have a concrete representation model.
+# The "resource" is a survey attempt/session populating a response set.
+class SurveyorController < ApplicationController
+  unloadable
+  include Surveyor::SurveyorControllerMethods
+
+  layout 'application'
+end
